17ce0b3f622cc945cadc10ffa2b2fb3ac34758f7
ynoa pedro <ynoa@smallbizsilverlining.com>
2022-03-27 15:28:34 -0300

    (chore) Initial commit

---
+++ api/Domain/Context/OnePieceDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace api.Domain;
+public class OnePieceDbContext : DbContext
+{
+    public CustomerDbContext()
+    {
+    }
+    public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+ 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Customer>()
+            .HasData(
+                new Customer { Id = 1, FirstName = "Jia", LastName = "Anu" },
+                new Customer { Id = 2, FirstName = "Naina", LastName = "Anu" },
+                new Customer { Id = 3, FirstName = "Sreena", LastName = "Anu" },
+                new Customer { Id = 4, FirstName = "Anu", LastName = "Viswan" }
+            );
+    }
+ 
+    public DbSet<Customer> Customer { get; set; }
+}
---
+++ api/Domain/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace api.Domain.Models;
+
+public interface IEntity
+{
+    long Id { get; set; }
+}
+
+public class Pirate : IEntity
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Crew { get; set; }
+    public string Rank { get; set; }
+    public List<Pirate> Foes { get; set; }
+}
